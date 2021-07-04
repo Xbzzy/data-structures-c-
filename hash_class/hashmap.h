d8ee08995acefd33e7f639d213c08beae2283361
Xbzzy <57384383+Xbzzy@users.noreply.github.com>
2021-07-04 20:36:14 +0800

    Update hashmap.h

--- hash_class/hashmap.h
+++ hash_class/hashmap.h
@@ -1,92 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#define null_value -1024
-typedef struct node
+#ifndef _HASHMAP_H_
+#define default_val -1024
+
+struct HashNode;
+typedef struct HashNode *Position;
+struct Map;
+typedef struct Map *HashMap;
+
+HashMap InitMap(int maxsize);
+Position CreateNode(int value);
+void Insert(HashMap hashmap, int value);
+Position Find(HashMap hashmap, int value);
+
+#endif /*_HASHMAO_H_*/
+
+struct HashNode
 {
-	int key;
-	int value;
-	struct node *next;
-}Node;
-typedef struct
+	int val;
+	struct HashNode *next;
+};
+struct Map
 {
-	int map_len;
-	Node *map;
-}hashmap;
-hashmap* create_map(int size)
+	int len;
+	struct HashNode *map;
+};
+HashMap InitMap(int maxsize)
 {
-	int i = 0, j = 0;
-	hashmap *hash_map = (hashmap*)malloc(sizeof(hashmap));
-	hash_map->map_len = size;
-	hash_map->map = (Node*)malloc(sizeof(Node)*hash_map->map_len);
-	for (int i = 0; i < hash_map->map_len; i++)
-	{
-		hash_map->map[i].value = null_value;
-		hash_map->map[i].next = NULL;
+	int i = 0;
+	HashMap hashmap = malloc(sizeof(struct Map));
+	if (hashmap == NULL){
+		printf("out of space!\n");
+		return NULL;
 	}
-	return hash_map;
+	hashmap->len = maxsize;
+	hashmap->map = malloc((sizeof(struct HashNode))*maxsize);
+	if (hashmap->map == NULL){
+		printf("out of space!\n");
+		return NULL;
+	}
+	for (int i = 0; i < maxsize; i++){
+		hashmap->map[i].val = default_val;
+		hashmap->map[i].next = NULL;
+	}
+	return hashmap;
 }
-void insert_data(hashmap* hash_map, int value)
+Position CreateNode(int site,int value)
 {
-	int site = abs(value%hash_map->map_len);
-	if (hash_map->map[site].value == null_value)
-	{
-		hash_map->map[site].key = site;
-		hash_map->map[site].value = value;
+	HashNode *node = malloc(sizeof(struct HashNode));
+	if (node == NULL){
+		printf("out of space!\n");
+		return NULL;
+	}
+	node->val = value;
+	node->next = NULL;
+}
+void Insert(HashMap hashmap, int value)
+{
+	int site;
+	site = abs(value%hashmap->len);
+	if (hashmap->map[site].val == default_val){
+		hashmap->map[site].val = value;
 	}
 	else
 	{
-		Node *next_node = (Node*)malloc(sizeof(Node));
-		next_node->key = site;
-		next_node->value = value;
-		next_node->next = NULL;
-		Node *temp = &(hash_map->map[site]);
-		while (temp->next != NULL)
-			temp = temp->next;
-		temp->next = next_node;
-	}
-}
-int search_data(hashmap* hash_map, int value)
-{
-	int site = abs(value % hash_map->map_len);
-	Node *temp = &(hash_map->map[site]);
-	while (temp != NULL)
-	{
-		if (temp->value == value)
-			return temp->key;
-		else temp = temp->next;
-	}
-	return -1;
-}
-void output_map(hashmap* hash_map)
-{
-	for (int i = 0; i < hash_map->map_len; i++)
-	{
-		Node* temp = &(hash_map->map[i]);
-		while (temp != NULL)
-		{
-			if (temp->value != null_value)
-				printf("%d ->", temp->value);
-			else printf("null");
-			temp = temp->next;
+		HashNode *tmp = &(hashmap->map[site]);
+		while (tmp->next != NULL){
+			tmp = tmp->next;
 		}
-		printf("\n");
+		Positon next_node = CreateNode(site, value);
+		tmp->next = next_node;
 	}
 }
-void main()
+Position Find(HashMap hashmap, int value)
 {
-	hashmap* hash_map=create_map(5);
-	output_map(hash_map);
-	printf("\n");
-	int datas[5] = { 12, 45, 23, 66, 18 };
-	for (int i = 0; i < 5; i++)
-		insert_data(hash_map, datas[i]);
-	output_map(hash_map);
-	printf("\n");
-	printf("map have 12?\n");
-	if (search_data(hash_map, 12) != -1)
-		printf("got it\n");
-	else printf("no\n");
-	system("pause");
-	return;
+	int site = abs(value % hashmap->len);
+	Position tmp = &(hashmap->map[site]);
+	while (tmp != NULL)
+	{
+		if (tmp->val == value)
+			return tmp;
+		else tmp = tmp->next;
+	}
+	return NULL;
 }
+
